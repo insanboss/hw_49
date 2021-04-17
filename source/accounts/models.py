26abd9f591c8b22727f24b54cd2547beebb86a8b
Insan <insanboss@gmail.com>
2021-04-17 16:56:29 +0600

    * Добавлена модель profile * Прогнаны миграции

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        verbose_name='Профиль пользователя',
+        related_name='profile',
+    )
+    birth_date = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='Дата рождения',
+    )
+    avatar = models.ImageField(
+        upload_to='avatars',
+        null=True,
+        blank=True,
+        verbose_name='Аватар'
+    )
+    github_profile = models.URLField(max_length=500, null=True, blank=True)
+    about_me = models.TextField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        db_table = 'profiles'
+        verbose_name = 'Профиль пользователя'
+        verbose_name_plural = 'Профили пользователей'
